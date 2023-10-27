9c6d5357fe9bd122b7411eaf991006511eede145
Skiexx <codeskiexx@yandex.ru>
2023-10-27 12:43:15 +0700

    Edited community plugins

--- options.lua
+++ options.lua
@@ -1,4 +1,3 @@
--- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
     -- set to true or false etc.
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
 }
