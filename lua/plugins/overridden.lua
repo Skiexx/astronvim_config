28ad75c41c4837a88fd4167fd89f9485ee418f16
Skiexx <CodeSkiexx@yandex.ru>
2024-06-13 20:36:39 +0700

    Added user config

---
+++ lua/plugins/overridden.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+      filtered_items = {
+        --visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".github",
+          ".gitignore",
+          "package-lock.json",
+          "yarn.lock",
+        },
+        never_show = { ".git" },
+      },
+    },
+    },
+  },
+}
