5bd96cb5699aaaa022a9adb82219a616440c525b
Skiexx <CodeSkiexx@yandex.ru>
2024-06-30 20:33:27 +0700

    Edited settings for neo-tree and gitsigns

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -3,19 +3,24 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
-      filtered_items = {
-        --visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          ".github",
-          ".gitignore",
-          "package-lock.json",
-          "yarn.lock",
+        filtered_items = {
+          --visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".gitignore",
+            "package-lock.json",
+            "yarn.lock",
+          },
+          never_show = { ".git" },
         },
-        never_show = { ".git" },
       },
     },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
     },
   },
 }
